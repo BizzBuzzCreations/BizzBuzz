030196a823caa08e32ff573c073a8014ff481fea
BizzBuzzCreations <bizzbuzzcreation@gmail.com>
2026-02-20 15:52:51 +0530

    for error log

--- actions/serverActions.js
+++ actions/serverActions.js
@@ -65,7 +65,7 @@ export async function sendMail({ name, email, subject, text, contact }) {
     console.error("Email send failed:", error);
     return {
       success: false,
-      message: "Failed to send message.",
+      message: error.message || "Failed to send message.",
     };
   }
 }
