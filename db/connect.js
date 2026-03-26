d65537098927be1d1c0f4b4eeabd43e1abccdad9
kushmal <kushmalarora017@gmail.com>
2026-03-26 13:04:00 +0530

    added admin page

--- db/connect.js
+++ db/connect.js
@@ -1,7 +1,14 @@
 import mongoose from "mongoose";
-
+import dns from "node:dns/promises";
+ 
 const connectDB = async () => {
   try {
+    // For only development use
+    if (process.env.NODE_ENV === "development") {
+      dns.setServers(["1.1.1.1", "8.8.8.8"]); // Cloudflare + Google
+      dns.setDefaultResultOrder("ipv4first");
+    }
+ 
     const conn = await mongoose.connect(`${process.env.MONGO_URI}`);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
@@ -9,5 +16,5 @@ const connectDB = async () => {
     process.exit(1);
   }
 };
-
+ 
 export default connectDB;
